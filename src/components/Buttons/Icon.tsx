8d35e8d985a259ee4cefe3b820f42b09cbc92dc3
Ricky Andrian <ricky.andrian@primecash.co.id>
2024-05-16 16:45:53 +0700

    hover effect button

--- src/components/Buttons/Icon.tsx
+++ src/components/Buttons/Icon.tsx
@@ -10,6 +10,7 @@ function ButtonIcon(props: Props) {
     <motion.a
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
+      whileHover={{ scale: 1.2 }}
       transition={{
         duration: 0.3,
         ease: [0, 0.71, 0.2, 1.01],
