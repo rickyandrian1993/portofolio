2745770fa6bd6eb29e9490646e55682b783077c2
Ricky Andrian <ricky.andrian@primecash.co.id>
2024-05-16 16:44:31 +0700

    animate button

--- src/components/Buttons/Icon.tsx
+++ src/components/Buttons/Icon.tsx
@@ -1,5 +1,5 @@
+import { motion } from "framer-motion";
 import { ReactNode } from "react";
-
 type Props = {
   children: ReactNode;
   url: string;
@@ -7,9 +7,25 @@ type Props = {
 
 function ButtonIcon(props: Props) {
   return (
-    <a href={props.url} target="_blank" className="btn btn-circle btn-primary">
+    <motion.a
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.3,
+        ease: [0, 0.71, 0.2, 1.01],
+        scale: {
+          type: "spring",
+          damping: 5,
+          stiffness: 100,
+          restDelta: 0.001,
+        },
+      }}
+      href={props.url}
+      target="_blank"
+      className="btn btn-circle btn-primary"
+    >
       {props.children}
-    </a>
+    </motion.a>
   );
 }
 
