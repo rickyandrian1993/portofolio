a22b4384746dfbeafa37336e8fa8c99af13785e8
Ricky Andrian <ricky.andrian@primecash.co.id>
2024-05-16 09:32:43 +0700

    Adding discord button

--- src/components/Buttons/Icon.tsx
+++ src/components/Buttons/Icon.tsx
@@ -3,13 +3,20 @@ import { ReactNode } from "react";
 type Props = {
   children: ReactNode;
   url: string;
+  tooltip: string;
 };
 
 function ButtonIcon(props: Props) {
   return (
-    <a href={props.url} target="_blank" className="btn btn-circle btn-primary">
-      {props.children}
-    </a>
+    <div className="tooltip tooltip-accent" data-tip={props.tooltip}>
+      <a
+        href={props.url}
+        target="_blank"
+        className="btn btn-circle btn-primary"
+      >
+        {props.children}
+      </a>
+    </div>
   );
 }
 
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,4 +1,5 @@
 import {
+  RiDiscordLine,
   RiFacebookBoxLine,
   RiGithubLine,
   RiInstagramLine,
@@ -9,17 +10,32 @@ import ButtonIcon from "../components/Buttons/Icon";
 function Contact() {
   return (
     <div className="flex justify-center gap-4">
-      <ButtonIcon url="https://www.instagram.com/_rickyandrian_/">
+      <ButtonIcon
+        url="https://www.instagram.com/_rickyandrian_/"
+        tooltip="Instagram"
+      >
         <RiInstagramLine />
       </ButtonIcon>
-      <ButtonIcon url="https://www.facebook.com/rickyandrian1993">
+      <ButtonIcon
+        url="https://www.facebook.com/rickyandrian1993"
+        tooltip="Facebook"
+      >
         <RiFacebookBoxLine />
       </ButtonIcon>
-      <ButtonIcon url="https://www.linkedin.com/in/andrian-ricky/">
+      <ButtonIcon
+        url="https://www.linkedin.com/in/andrian-ricky/"
+        tooltip="LinkedIn"
+      >
         <RiLinkedinLine />
       </ButtonIcon>
-      <ButtonIcon url="https://github.com/rickyandrian1993">
+      <ButtonIcon url="https://github.com/rickyandrian1993" tooltip="Github">
         <RiGithubLine />
+      </ButtonIcon>
+      <ButtonIcon
+        url="https://discord.com/users/ricky_andrian"
+        tooltip="Discord"
+      >
+        <RiDiscordLine />
       </ButtonIcon>
     </div>
   );
